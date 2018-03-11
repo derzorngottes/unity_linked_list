b5330254d19a9450731d691de8b36cd0e1e177b6
Amy Conchie <amy.conchie@gmail.com>
2018-03-11 15:46:30 -0700

    Initial commit, add startup and linkedlist scripts

---
+++ Assets/linkedlist.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkedList : MonoBehaviour {
+	public Node head;
+	public Node current;
+	public int count;
+	public int maxLength;
+
+	// Use this for initialization
+	void Start () {
+		Node head = new Node();
+		Node tail = head;
+		maxLength = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddAtEnd(string value) {
+		if (count >= maxLength) {
+			// TODO display warning text to user
+			return;
+		}
+
+		Node newNode = new Node (value);
+
+		// create Node node
+		current.next = newNode;
+		newNode.next = null;
+		count++;
+
+		// create physical node
+		GameObject newNodeObj = MakeNewNode(value);
+	}
+
+	public void RemoveFromEnd() {
+		// iterate over LL until next == current
+		Node curNode = head;
+		while (curNode.next != current) {
+			curNode = curNode.next;
+		}
+
+	}
+
+
+	/*
+	 *  UNITY SPECIFIC FUNCTIONS
+	 *  For implementing the structure in 3D space
+	 */
+
+	public GameObject MakeNewNode(string value) {
+		// create sphere to represent node
+		GameObject newNode = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		// create text mesh to display value and attach to node
+		TextMesh newNodeValue = new TextMesh ();
+		newNodeValue.text = value;
+		//newNodeValue.transform.parent = newNode;
+		// size and place node on map according to LL position
+		newNode.transform.localScale += new Vector3(5.0f, 5.0f, 5.0f);
+		newNode.transform.position = new Vector3 (current.position.x + 10.0f, current.position.y, current.position.z);
+		return newNode;
+	}
+}
+
+public class Node : MonoBehaviour {
+	string Value;
+	public Node next;
+	public Vector3 position;
+}
---
+++ Assets/startup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startup : MonoBehaviour {
+
+	public Terrain terrainMap;
+
+	// Use this for initialization
+	void Start () {
+		CreateTerrain ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CreateTerrain() {
+		GameObject TerrainObj = new GameObject("TerrainObj");
+
+		TerrainData _TerrainData = new TerrainData();
+
+		// terrain is 10 x 10, try changing to 50 x 50 for a much larger city
+		_TerrainData.size = new Vector3(10, 600, 10);
+		_TerrainData.heightmapResolution = 512;
+		_TerrainData.baseMapResolution = 1024;
+		_TerrainData.SetDetailResolution(1024, 16);
+
+		TerrainCollider _TerrainCollider = TerrainObj.AddComponent<TerrainCollider>();
+		terrainMap = TerrainObj.AddComponent<Terrain>();
+
+		_TerrainCollider.terrainData = _TerrainData;
+		terrainMap.terrainData = _TerrainData;
+	}
+}
